e074f12fe9a8c393d295812dd3fd74ace4e763c5
ibrahimersahin <46766229+ibrahimersahin@users.noreply.github.com>
2019-01-19 12:03:38 -0600

    Add multiply method.

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,4 +8,8 @@ public class Calculator {
         double result = num1-num2;
         return num1-num2;
     }
+    
+    public static double multiply (double num1 , double num2){
+        return num1*num2;
+    }
 }
